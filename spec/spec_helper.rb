e8a06e50d08c633232f20afcc4c5eda5a61bc775
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2024-09-24 19:44:54 -0300

    test: config rspec for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
-require "gunter"
+# load simplecov
+require 'simplecov'
+SimpleCov.start if ENV['COVERAGE']
+
+# require all files in the support directory
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.filter_run_when_matching :focus
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
