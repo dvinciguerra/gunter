35df32b49db1f10300e23fa6268cd70757975202
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2024-09-24 19:43:43 -0300

    feat: change models to return proper error

--- lib/gunter/models.rb
+++ lib/gunter/models.rb
@@ -11,6 +11,8 @@ module Gunter
       # @param name [Symbol] the model name
       def model(name)
         Gunter::Model.const_get(name.to_s.split('_').map(&:capitalize).join)
+      rescue NameError
+        raise Gunter::ModelError, "Model '#{name}' not found"
       end
     end
   end
